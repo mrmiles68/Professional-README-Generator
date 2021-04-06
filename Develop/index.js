4587440bdf5f4a6002b415303715ee3a604e7b57
mark <mark@marks-MacBook-Pro.local>
2021-04-05 20:50:23 -0700

    writefile successful

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,41 @@
 // TODO: Include packages needed for this application
-
+ const fs = require('fs');
+const prompt = require("prompt-sync")();
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = ["Project Name" , "Description" , "Badges?", "Install Instructions?",
+ "Usage", "Support contacts?", "Future Roadmap?", "Open to Contributions?", "Authors and Acknowledgment?", "Licensing?"];
+let answers = [];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function getAnswers(newREADME, data) {
+
+    for (i=0; i<questions.length; i++){
+
+    answers[i] = prompt(questions[i] + " ");
+        }}
+    
+function writeToFile() {
+
+    for (i=0; i<questions.length; i++){
+   
+    const saveItem = `${ questions[i] }\n${ answers[i] }\n \n`;
+   
+    fs.appendFile("newREADME.md", saveItem, (err) => {
+        if (err) {
+          console.error(err);
+          return
+    }});
+
+
+
+}}
+
+
+
+getAnswers()
+
+writeToFile()
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
---
+++ Develop/newREADME.md
@@ -0,0 +1,30 @@
+Description
+w
+ 
+Usage
+t
+ 
+Open to Contributions?
+i
+ 
+Licensing?
+p
+ 
+Authors and Acknowledgment?
+o
+ 
+Badges?
+e
+ 
+Support contacts?
+y
+ 
+Project Name
+q
+ 
+Future Roadmap?
+u
+ 
+Install Instructions?
+r
+ 
